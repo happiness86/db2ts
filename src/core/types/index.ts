7596ca31ea4741c5b66bccddb47092ff3b6b29bf
liang.hong <39-liang.hong@users.noreply.gitlab.ubixai.com>
2024-11-06 11:34:23 +0800

    docs: add annotation

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -5,8 +5,18 @@ export type DbDrivers = 'mysql'
 export type DbConnectOptions = PoolOptions
 
 export interface OutputOptions {
+  /**
+   * @description 生成ts文件或者dts文件
+   */
   fileType?: 'ts' | 'dts'
-  singleFile?: boolean // 是否在一个文件内
+  /**
+   * @description 是否将所有的数据生成在一个文件内
+   * @default false
+   */
+  singleFile?: boolean
+  /**
+   * @description 生成的文件的存储路径，绝对路径
+   */
   dir?: string
 }
 
