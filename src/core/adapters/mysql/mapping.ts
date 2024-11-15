b8e8ea17c04034cb3962175b36d56e1dc57dc1fb
cocod <39-liang.hong@users.noreply.gitlab.ubixai.com>
2024-11-15 18:03:57 +0800

    feat: support match date type

--- src/core/adapters/mysql/mapping.ts
+++ src/core/adapters/mysql/mapping.ts
@@ -3,7 +3,7 @@ import type mysql from 'mysql2'
 export type MysqlTypes = mysql.Types
 
 const numberReg = /^(?:double|int|float|decimal|long|short|tiny|bigint)/
-const stringReg = /^(?:varchar|json|text|datetime|timestamp|string|varString)/
+const stringReg = /^(?:varchar|json|text|datetime|date|timestamp|string|varString)/
 
 export function dataTypeMapToTs(type: string): string {
   if (numberReg.test(type)) {
